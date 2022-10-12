0a32c761daf57f3322bd56b03a707fe12fd6339f
maxi <maximiliano.jara_2803@hotmail.cl>
2022-10-11 21:09:14 -0300

    CursoPython workspace variables

---
+++ cursoPython/WorkSpace/variables/variables.py
@@ -0,0 +1,20 @@
+#if 5>3:
+   # print("5 es mayor a 3")
+#if 3>5:
+    #print("esto esto no  se  imprime")
+
+  # lo siguente es una variable
+x=5
+y="perro chuleta"
+#esto es como hacer una referencia  llamar una variable 
+print(x,y)
+
+correo="perrochuleta@gmail.com"
+#print(correo)
+#FORMA DE VARIABLES
+mivar="chuleta"
+
+_usuario="chancho"
+#FORMA DE CAMELCASE DE VARIABLE
+Va="JAMOM"
+print(mivar ,_usuario,Va)
