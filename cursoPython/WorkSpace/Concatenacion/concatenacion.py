e09f0d8278b3dd9f694aee34596a648184b9d3db
maxi <maximiliano.jara_2803@hotmail.cl>
2022-10-13 23:15:51 -0300

    concatenacion  tipo de datos

---
+++ cursoPython/WorkSpace/Concatenacion/concatenacion.py
@@ -0,0 +1,37 @@
+#identacion
+#if 5>3:
+   # print("5 es mayor a 3")
+#if 3>5:
+    #print("esto esto no  se  imprime")
+#--------------------------------------------------------
+  # lo siguente es una variable
+x=5
+y="perro chuleta"
+#esto es como hacer una referencia  llamar una variable 
+#print(x,y)
+
+correo="perrochuleta@gmail.com"
+#print(correo)
+#FORMA DE VARIABLES
+#mivar="chuleta"
+
+#_usuario="chancho"
+#FORMA DE CAMELCASE DE VARIABLE
+#Va="JAMOM"
+#print(mivar ,_usuario,Va)
+#---------------------------------------------------------------------
+#varibles Multiples
+#otra forma una de tener mutiples variables en una sola linea 
+_mi_var='perro chuleta'
+MiVAR='PERRO CHANCHO CHULETA'
+a,b,c='lala','lele','lili'
+#print(_mi_var,MiVAR,a,b,c)
+#otra manera  hayan muchas variables que tienga un  mismo valor
+valor1=valor2=valor3='chancho feliz'
+#print(valor1,valor2,valor3)
+
+#----------------------------------------------------------------
+#concatenacion de Strings
+inicio='hola'
+final='mundo'
+print(inicio + final)
