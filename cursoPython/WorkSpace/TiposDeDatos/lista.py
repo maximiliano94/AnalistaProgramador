8378ef3ee57b24dc57a4909151e9031d0f4b3673
maxi <maximiliano.jara_2803@hotmail.cl>
2022-10-15 00:51:23 -0300

    tipo de datos-manejo de LISTA/Array

---
+++ cursoPython/WorkSpace/TiposDeDatos/lista.py
@@ -0,0 +1,59 @@
+#identacion
+#if 5>3:
+   # print("5 es mayor a 3")
+#if 3>5:
+    #print("esto esto no  se  imprime")
+#--------------------------------------------------------
+  # lo siguente es una variable
+x=5
+y="perro chuleta"
+#esto es como hacer una referencia  llamar una variable 
+#print(x,y)
+
+correo="perrochuleta@gmail.com"
+#print(correo)
+#FORMA DE VARIABLES
+#mivar="chuleta"
+
+#_usuario="chancho"
+#FORMA DE CAMELCASE DE VARIABLE
+#Va="JAMOM"
+#print(mivar ,_usuario,Va)
+#---------------------------------------------------------------------
+#varibles Multiples
+#otra forma una de tener mutiples variables en una sola linea 
+_mi_var='perro chuleta'
+MiVAR='PERRO CHANCHO CHULETA'
+a,b,c='lala','lele','lili'
+#print(_mi_var,MiVAR,a,b,c)
+#otra manera  hayan muchas variables que tienga un  mismo valor
+valor1=valor2=valor3='chancho feliz'
+#print(valor1,valor2,valor3)
+
+#----------------------------------------------------------------
+#concatenacion de Strings
+inicio='hola'
+final='mundo'
+#print(inicio + final)
+#-----------------------------------------------------------------
+#tipos de datos introduccion de la seccion String y numeros 
+palabra='hola mundo con comillas simples' #String
+fraces="hola mundo comilla dobles"
+#print(palabra,fraces)
+#-----------------------------------------------------
+entero=20 # esto es un interger
+conDecimales=20.5 # esto es un flotatante
+complejo=1j #numeros complejos
+#print(entero,conDecimales,complejo,palabra,fraces)
+#----------------------------------------------------------
+#lista
+lista=[1,2,3]
+#metodos para trabajar con listas 
+#para  agregar un elemento 
+lista.append(4)
+#limpia la lista
+#lista.clear()
+#metodo para copiar una lista
+lista2=lista.copy()
+
+print(lista,lista2)
