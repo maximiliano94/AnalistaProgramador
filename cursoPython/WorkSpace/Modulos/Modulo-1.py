193335c450bb1ecb49622c0c1fb706678be5e5c6
maxi <maximiliano.jara_2803@hotmail.cl>
2022-12-03 02:44:50 -0300

    seccion modulos gestionArchivos

--- cursoPython/WorkSpace/Modulos/Modulo-1.py
+++ cursoPython/WorkSpace/Modulos/Modulo-1.py
@@ -1,4 +1,40 @@
+#----------------------------------------------
+#           importar modulos
+#----------------------------------------------
+'''''
 import Modulos
 
 print(Modulos.mascotas)
 Modulos.saludo("Maxi")
+'''
+#-----------------------------------------------
+#              renombrar modulos
+#-----------------------------------------------
+'''
+import Modulos as xs
+
+print(xs.mascotas)
+xs.saludo("Maxi")
+'''
+#------------------------------------------------
+#   seleccionar lo importado 
+#-----------------------------------------------
+'''''
+from Modulos import saludo
+saludo("Maxi")
+'''''
+#-----------------------------------------------
+#                 CamelCase
+#----------------------------------------------
+import modulos as xs
+from camelcase import CamelCase
+
+print(xs.mascotas)
+xs.saludo('Nicolas')
+
+c = CamelCase()
+s = 'esta oración necesita CamelCase!'
+
+camelcased = c.hump(s)
+print(camelcased)
+# para instalar modulos pip intall desintalar pip unintaller
