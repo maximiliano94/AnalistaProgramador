c0ca0828cd645c0a3d7e4e623332c154db62b92e
maxi <maximiliano.jara_2803@hotmail.cl>
2022-12-29 03:58:04 -0300

    seccion de ejercicios

---
+++ cursoPython/WorkSpace/ejerciciosV2/ejercicio1.py
@@ -0,0 +1,9 @@
+#multiplica 2 numeros sin el sin el simbolo de  muñtiplicar
+a=4
+b=8
+resultado=0
+
+for x in range(a):
+    resultado+=b
+
+print(resultado)
---
+++ cursoPython/WorkSpace/ejerciciosV2/ejercicio2.py
@@ -0,0 +1,6 @@
+# solictar nombre  y apellido  e invertirlo
+nombre='maximiliano'
+apellido='jara'
+concatenacion=nombre +' '+ apellido
+
+print(concatenacion[::-1])
