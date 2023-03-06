019dd937ec68b8cfdff1c64702fa918473f996c1
maxi <maximiliano.jara_2803@hotmail.cl>
2023-03-06 17:41:31 -0300

    cursos python
    harr

---
+++ harward/holamundo.py
@@ -0,0 +1,15 @@
+#print("hola mundo")
+#suma de dos numeros solicitados
+# programa seccuencial
+num1=int(input("ingrese el  mumero 1....."))
+num2=int(input("ingrese el  mumero 2....."))
+resultado=int
+resultado=num1+num2
+print("el resultado es:",resultado)
+
+
+
+
+
+
+
